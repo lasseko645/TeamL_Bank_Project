dc594984a90c31dc3bbf7f1a18c28213a701e129
niel905g <niel905g@edu.easj.dk>
2017-11-29 08:30:08 +0100

    Kode i konti, opsparing og lønkonto

--- src/Loenkonto.java
+++ src/Loenkonto.java
@@ -3,4 +3,54 @@ public class Loenkonto extends Konti {
     //Klassen Loenkonto:
     //Håndterer data om Lønkonti. Skal kunne ting indenfor de parametre vi har fået.
     //Indsætte penge, Udtrække penge, Sende penge til Opsparing, etc..
+
+    private double deposit = 0;
+    private double withDraw = 0;
+    private double limit = 0;
+
+    private Loenkonto(String typeName) {
+        super(typeName);
+    }
+
+    private Loenkonto(int acc_Regno, double acc_No, String typeName) {
+        super(acc_Regno, acc_No, typeName);
+    }
+
+    public Loenkonto(String typeName, double deposit, double withDraw, double limit) {
+        super(typeName);
+        this.deposit = deposit;
+        this.withDraw = withDraw;
+        this.limit = limit;
+    }
+
+    public Loenkonto(int acc_Regno, double acc_No, String typeName, double deposit, double withDraw, double limit) {
+        super(acc_Regno, acc_No, typeName);
+        this.deposit = deposit;
+        this.withDraw = withDraw;
+        this.limit = limit;
+    }
+
+    public double getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(double deposit) {
+        this.deposit = deposit;
+    }
+
+    public double getWithDraw() {
+        return withDraw;
+    }
+
+    public void setWithDraw(double withDraw) {
+        this.withDraw = withDraw;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
 }
--- src/Opsparingskonto.java
+++ src/Opsparingskonto.java
@@ -3,4 +3,38 @@ public class Opsparingskonto extends Konti {
     //Klassen Opsparingskonto:
     //Håndterer data om Opsparingskonti. Skal kunne ting indenfor de parametre vi har fået.
     //Modtage penge, IKKE være i minus, etc..
+
+    private double deposit = 0;
+    private double withDraw = 0;
+    private double limit = 0;
+
+    public Opsparingskonto(String typeName) {
+        super(typeName);
+    }
+
+    public Opsparingskonto(int acc_Regno, double acc_No, String typeName) {
+        super(acc_Regno, acc_No, typeName);
+    }
+
+    public Opsparingskonto(String typeName, double deposit, double withDraw, double limit) {
+        super(typeName);
+        this.deposit = deposit;
+        this.withDraw = withDraw;
+        this.limit = limit;
+    }
+
+    public Opsparingskonto(int acc_Regno, double acc_No, String typeName, double deposit, double withDraw, double limit) {
+        super(acc_Regno, acc_No, typeName);
+        this.deposit = deposit;
+        this.withDraw = withDraw;
+        this.limit = limit;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
 }
