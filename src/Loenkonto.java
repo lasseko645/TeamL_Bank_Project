05319b956ac596f1e5a40b28bd1caeb3198212ec
Benjamin <benjamin@smoerfugl.dk>
2017-11-30 10:39:19 +0100

    i fixed it !!

--- src/Loenkonto.java
+++ src/Loenkonto.java
@@ -1,56 +1,24 @@
+
+/**
+ * denne klasse skal være supklasse af en klasse some hedder Konti
+ * ideen med denne klasse er at den skal kunne kontrollere at det er en lønkonto når vi gør brug af nogle metoder
+ * det som en lønkonto skal kunne gå i minus, som den eneste form for konto i vores program
+ *
+ * @author jokkol2
+ * @version 1
+ * @since 2017-30-11
+ */
+
 public class Loenkonto extends Konti {
 
-    //Klassen Loenkonto:
-    //Håndterer data om Lønkonti. Skal kunne ting indenfor de parametre vi har fået.
-    //Indsætte penge, Udtrække penge, Sende penge til Opsparing, etc..
+    /**
+     * denne klasse nedarver følgendene variabler
+     * double acc_No
+     * int acc_RegNo
+     * double acc_Sum
+     * String acc_Navn
+     *
+     */
 
-    private double deposit = 0;
-    private double withDraw = 0;
-    private double limit = 0;
 
-    private Loenkonto(String typeName) {
-        super(typeName);
-    }
-
-    private Loenkonto(int acc_Regno, double acc_No, String typeName) {
-        super(acc_Regno, acc_No, typeName);
-    }
-
-    public Loenkonto(String typeName, double deposit, double withDraw, double limit) {
-        super(typeName);
-        this.deposit = deposit;
-        this.withDraw = withDraw;
-        this.limit = limit;
-    }
-
-    public Loenkonto(int acc_Regno, double acc_No, String typeName, double deposit, double withDraw, double limit) {
-        super(acc_Regno, acc_No, typeName);
-        this.deposit = deposit;
-        this.withDraw = withDraw;
-        this.limit = limit;
-    }
-
-    public double getDeposit() {
-        return deposit;
-    }
-
-    public void setDeposit(double deposit) {
-        this.deposit = deposit;
-    }
-
-    public double getWithDraw() {
-        return withDraw;
-    }
-
-    public void setWithDraw(double withDraw) {
-        this.withDraw = withDraw;
-    }
-
-    public double getLimit() {
-        return limit;
-    }
-
-    public void setLimit(double limit) {
-        this.limit = limit;
-    }
 }
